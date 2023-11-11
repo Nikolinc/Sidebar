4caf44660e3e73b06b5a87f2f8259a7fe4b27c8d
Nikolinc <tsigulskynikita@gmail.com>
2023-11-11 23:04:01 +0500

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
+import router from './router'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/home";
 import ErrorPage from "../pages/error/error";
+import RootPage from "../pages/rootPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <RootPage />,
     errorElement: <ErrorPage />,
     children: [
       {
